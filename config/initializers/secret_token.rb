49950db26bbabb100be617d0b6e3837c322dc314
Andrea Jessup <jessup.andrea@gmail.com>
2012-02-13 20:27:19 -0600

    First commit for craazy cats

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrazyCats::Application.config.secret_token = 'ea7893f0548d755163edd3af6da49b4f4c69ddb74361125cd94292d2124bc3c3d1c4570f4b79653f13df74855592f09a003bd9da66e29535ba20dba7bbb4f020'
